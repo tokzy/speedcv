118d84f192e7f93d8cc2af58efb437a33830cd40
netsage <netsage23@gmail.com>
2021-01-01 21:06:15 +0000

    save

--- public/templates/cv5.php
+++ public/templates/cv5.php
@@ -1,300 +1,378 @@
+<?php
+ob_start();
+require_once '../../src/class/apiclass.php';
+if(isset($_GET['pid'])):
+
+$api = new Apiclass();	
+$pid = $api->cleanInputs($_GET['pid']);
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1" /> -->
-	<title>CV Template</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
-	<link href='https://fonts.googleapis.com/css?family=Tahoma' rel='stylesheet'>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1" /> -->
+<title>CV Template</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<link href='https://fonts.googleapis.com/css?family=Tahoma' rel='stylesheet'>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
 </head>
 <body>
-	<div class="wrapper">
-		<div class="resume">
-			<!-- TOP CONTAINER-->
-				<div class= "container cv5-top">
-					<!-- user image and name-->
-					<div class=" cv5-user">
-						<!-- user image -->
-						<div class="user-img">
-							<img src="./img/images.jpg" alt="cv-img">
-						</div>
+<div class="wrapper">
+<div class="resume">
+<?php
+$pdetails = $api->getpd($pid);
+?>
+<!-- TOP CONTAINER-->
+<div class= "container cv5-top">
+<!-- user image and name-->
+<div class=" cv5-user">
+<!-- user image -->
+<?php if(empty($pdetails['profile'])):else:?>
+<div class="user-img">
+<img src="../images/<?php echo $pdetails['profile'];?>" alt="cv-img">
+</div>
+<?php endif;?>
+<!-- user name -->
+<?php if(empty($pdetails['name'])):else:?>
+<div class="user-name">
+<h1 class="bold"><?php echo strtoupper($pdetails['name']);?></h1>
+</div>
+<?php endif;?>
+</div>
+<!-- user image and name end-->
+<!-- other personal info -->
+<div class="cv5-opi ">
+<div class="cv5-personal-info ">
+<?php if(empty($pdetails['phone'])):else:?>
+<div class="opi-item cv5-opi-item cv5-p-i">
+<p class="opi-item-title m-r"><img src="./img/android-b.png"></p>
+<p class="disp-i-b"><?php echo $pdetails['phone'];?></p>
+</div>
+<?php endif;?>
 
-						<!-- user name -->
-						<div class="user-name">
-							<h1 class="bold">JOHN DOE</h1>
-						</div>
-					</div>
-					<!-- user image and name end-->
-				<!-- other personal info -->
-				<div class="cv5-opi ">
-					<div class="cv5-personal-info ">
-						<div class="opi-item cv5-opi-item cv5-p-i">
-							<p class="opi-item-title m-r"><img src="./img/android-b.png"></p>
-							<p class="disp-i-b">000-000-000-000</p>
-						</div>
+<?php if(empty($pdetails['email'])):else:?>
+<div class="opi-item cv5-opi-item cv5-p-i">
+<p class="opi-item-title m-r"><img src="./img/secured-letter-b.png"></p>
+<p class="disp-i-b"><?php echo $pdetails['email'];?></p>
+</div>
+<?php endif;?>
 
-						<div class="opi-item cv5-opi-item cv5-p-i">
-							<p class="opi-item-title m-r"><img src="./img/secured-letter-b.png"></p>
-							<p class="disp-i-b">johndoe@gmail.com</p>
-						</div>
+<?php if(empty($pdetails['address'])):else:?>
+<div class="opi-item cv5-opi-item cv5-p-i">
+<p class="opi-item-title m-r"><img src="./img/user-location-b.png"></p>
+<p class="disp-i-b"><?php echo $pdetails['address'];?></p>
+</div>
+<?php endif;?>
 
-						<div class="opi-item cv5-opi-item cv5-p-i">
-							<p class="opi-item-title m-r"><img src="./img/user-location-b.png"></p>
-							<p class="disp-i-b">number, Street Name, City, State, Country</p>
-						</div>
+<?php if(empty($pdetails['website'])):else:?>
+<div class="opi-item cv5-opi-item cv5-p-i">
+<p class="opi-item-title m-r"><img src="./img/globe-b.png"></p>
+<p class="disp-i-b"><?php echo $pdetails['website'];?></p>
+</div>
+<?php endif;?>
+</div>
+</div>
+<!-- other personal info end-->
+</div>
+<!-- TOP CONTAINER END -->
 
-						<div class="opi-item cv5-opi-item cv5-p-i">
-							<p class="opi-item-title m-r"><img src="./img/globe-b.png"></p>
-							<p class="disp-i-b">www.johndoe.com</p>
-						</div>
-					</div>
-				</div>
-				<!-- other personal info end-->
-				</div>
-			<!-- TOP CONTAINER END -->
+<!--Bigger container for Other details -->
+<div class="row-vm">
+<!-- Bigger container at the left -->
+<div class="container cv5-left general-info l-float">
+<!--Objective -->
+<?php
+$title = "Objective";
+$obj = $api->getheadersbytitle($title);
+$objectives = $api->getAllObjBypid($pid);
+if(count($objectives) <= 0):else:
+?>
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $obj['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<div class="info-text">
+<div class="objective-info">
+<p>
+<?php
+foreach($objectives as $objective):
+?>
+<p><?php echo nl2br($objective['obj']);?></p>
+<?php
+endforeach;	
+?>
+</p>
+</div>
+</div>
+</div>
+<!--Objective ends-->
+<?php endif;?>
 
-			<!--Bigger container for Other details -->
-			<div class="row-vm">
-				<!-- Bigger container at the left -->
-				<div class="container cv5-left general-info l-float">
-					<!--Objective -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Objective</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text">
-							<div class="objective-info">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur.
-								</p>
-							</div>
-						</div>
-					</div>
-					<!--Objective ends-->
+<?php 
+$title4 = "Experience";
+$obj4 = $api->getheadersbytitle($title4);
+$experiences = $api->getExperience($pid);
+if(count($experiences) <= 0):else:
+?>
+<!--Experience -->
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $obj4['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<?php foreach($experiences as $exp):?>
+<div class="info-text">
+<div class="experience-info ">
+<p class="small-bold"><?php echo ucwords($exp['companyName']);?></p>
+<p><?php echo $exp['jobTitle'];?></p>
+<p><?php echo $exp['startDate'];?> - <?php echo ucwords($exp['endDate']);?></p>
+<p class="pad-top"><?php echo nl2br($exp['details']);?></p>
+</div>
+</div>
+<?php endforeach;?>
+</div>
+<?php endif;?>
+<!--Experience end-->
+<?php 
+$title5 = "Project";
+$obj5 = $api->getheadersbytitle($title5);
+$projects = $api->getAllproject($pid);
+if(count($projects) <= 0):else:
+?>
+<!--Project -->
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $obj5['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<?php foreach($projects as $project):?>
+<div class="info-text">
+<div class="project-info ">
+<p class="small-bold"><?php echo ucwords($project['title']);?></p>
+<p><?php echo nl2br($project['description']);?></p>
+</div>
+</div>
+<?php endforeach;?>
+</div>
+<!--Project end-->
+<?php endif;?>
 
-					<!--Experience -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Experience</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text">
-							<div class="experience-info ">
-								<p class="small-bold">Company/Name</p>
-								<p>Job/Title</p>
-								<p>Start date - End date</p>
-								<p class="pad-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-							</div>
-						</div>
-					</div>
-					<!--Experience end-->
+<?php 
+$title6 = "Reference";
+$obj6 = $api->getheadersbytitle($title6);
+$referes = $api->getAllreference($pid);
+if(count($referes) <= 0):else:
+?>
+<!--Reference -->
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $obj6['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<?php foreach($referes as $ref):?>
+<div class="info-text">
+<div class="reference-info ">
+<p>
+<span style="font-size:20px;"><?php echo $ref['refereeName'];?></span><br/>
+<?php echo $ref['jobTitle'];?><br/>
+<?php echo $ref['companyName'];?><br/>
+<?php echo $ref['email'];?><br/>
+<?php echo $ref['phone'];?><br/>
+</p>
+</div>
+</div>
+<?php endforeach;?>
+</div>
+<!--Reference end-->
+<?php endif;?>
+<?php 
+//$title6 = "Reference";
+//$obj6 = $api->getheadersbytitle($title6);
+$misc = $api->getMisc($pid);
+if(count($misc) <= 0):else:
+foreach($misc as $miscell):    
+$id = $api->getheadersbyheaderId($miscell['headingId']);
+?>
+<!--Reference -->
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $id['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<div class="info-text">
+<div class="reference-info ">
+<?php
+$values = $api->getAllValues($miscell['headingId'],$pid);
+foreach($values as $value):
+?>
+<p><?php echo $value['value'];?></p>
+<?php endforeach;?>
+</div>
+</div>
 
-					<!--Project -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Pojects</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text">
-							<div class="project-info ">
-								<p class="small-bold">Title</p>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</p>
-							</div>
-						</div>
-					</div>
-					<!--Project end-->
+</div><br/>
+<!--Reference end-->
+<?php endforeach;endif;?>
+</div>
+<!-- Bigger container at the left end-->
 
-					<!--Reference -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Reference</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text">
-							<div class="reference-info ">
-								<p>Lorem ipsum dolor sit amet.</p>
-							</div>
-						</div>
-					</div>
-					<!--Reference end-->
-				</div>
-				<!-- Bigger container at the left end-->
+<!-- Bigger container at the right -->
+<div class="container cv5-right general-info r-float">
+<!--Personal Info -->
+<div class="gi">
+<div class="info-header">
+<h2>Personal Information</h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<div class="info-text">
+<div class="objective-info">
+<div class="cv5-extra-personal-info ">
+<?php if(empty($pdetails['dob']) && empty($pdetails['gender']) && empty($pdetails['religion'])): else:?>
+<?php if(empty($pdetails['dob'])):else:?>
+<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
+<p class="cv5-opi-item-title">Date of Birth:</p>
+<p><?php echo $pdetails['dob'];?></p>
+</div>
+<?php endif;?>
 
-				<!-- Bigger container at the right -->
-				<div class="container cv5-right general-info r-float">
-					<!--Personal Info -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Personal Information</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text">
-							<div class="objective-info">
-								<div class="cv5-extra-personal-info ">
-									<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
-										<p class="cv5-opi-item-title">Date of Birth:</p>
-										<p>01-01-0001</p>
-									</div>
+<?php if(empty($pdetails['gender'])):else:?>
+<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
+<p class="cv5-opi-item-title">Gender:</p>
+<p><?php echo $pdetails['gender'];?></p>
+</div>
+<?php endif;?>
 
-									<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
-										<p class="cv5-opi-item-title">Gender:</p>
-										<p>Unknown</p>
-									</div>
+<?php if(empty($pdetails['religion'])):else:?>
+<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
+<p class="cv5-opi-item-title">Religion</p>
+<p><?php echo $pdetails['religion'];?></p>
+</div>
+<?php endif;?>
+</div> 
+<?php endif;?>
+</div>
+</div>
+</div>
+<!--Personal Info ends-->
 
-									<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
-										<p class="cv5-opi-item-title">Religion</p>
-										<p>Still thinking</p>
-									</div>
+<?php
+$title2 = "Education";
+$obj2 = $api->getheadersbytitle($title2);
+$edus = $api->getAllEduBypid($pid);
+if(count($edus) <= 0):else:
+?>
+<!--Education -->
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $obj2['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<div class="info-text education cv3-edu">
+<div class="row" style="padding-left:10px;">
+<?php foreach($edus as $edu):?>
+<div class="column">
+<h2><?php echo $edu['course'];?></h2>
+<p><?php echo $edu['school'];?></p>
+<p><?php echo $edu['grade'];?></p>
+<?php echo $edu['year'];?>
+</div>
+<?php endforeach;?>
+</div>
+</div>
+</div>
+<!--Education end-->
+<?php endif;?>
 
-									<div class="opi-item cv5-opi-item cv5-extra-p-i-div">
-										<p class="cv5-opi-item-title">Marital Status:</p>
-										<p>Confused</p>
-									</div>
-								</div> 
-							</div>
-						</div>
-					</div>
-					<!--Personal Info ends-->
+<?php
+$title3 = "Skills";
+$obj3 = $api->getheadersbytitle($title3);
+$skills = $api->getAllSkills($pid);
+if(count($skills) <= 0):else:
+?>
+<!--Skills -->
+<div class="gi">
+<div class="info-header">
+<h2><?php echo $obj3['title'];?></h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<div class="info-text">
+<div class="skills-info cv5-skills-info">
+<div class="row" style="padding-left:10px;">
+<?php foreach($skills as $skill):?>
+<div>
+<div class="skills-info-li">
+<p class="skill-name"><?php echo $skill["skills"];?></p>
+<div class="sp cv5-sp">
+<?php if($skill['level'] == "1"):?>
+<div class="sb cv5-sb w-20"></div>
+<?php elseif($skill['level'] == "2"):?>
+<div class="sb cv5-sb w-40"></div>
+<?php elseif($skill['level'] == "3"):?>
+<div class="sb cv5-sb w-60"></div>
+<?php elseif($skill['level'] == "4"):?>
+<div class="sb cv5-sb w-80"></div>
+<?php elseif($skill['level'] == "5"):?>
+<div class="sb cv5-sb w-100"></div>
+<?php endif;?>	
+</div>
+</div>
+</div>
+<?php endforeach;?>
 
-					<!--Education -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Education</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text education cv3-edu">
-							<table>
-							  <tr class="small-bold">
-							    <th>Course/Degree</th>
-							    <th>Course/Degree</th>
-							  </tr>
-							  <tr>
-							    <td>School/university name</td>
-							    <td>School/university name</td>
-							  </tr>
-							  <tr>
-							    <td>Grade/Score</td>
-							    <td>Grade/Score</td>
-							  </tr>
-							  <tr>
-							   <td>Year</td>
-							   <td>Year</td>
-							  </tr>
-							</table>
-					</div>
-					</div>
-					<!--Education end-->
+</div>
+</div>
+</div>
+</div>
+<!--Skills end-->
+<?php endif;?>
 
-					<!--Skills -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Skills</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text">
-							<div class="skills-info cv5-skills-info">
-								<table>
-									<tr>
-										<td class="skills-info-li">
-												<p class="skill-name">Lorem ipsum dolor</p>
-												<div class="sp cv5-sp">
-													<div class="sb cv5-sb w-40"></div>
-												</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="skills-info-li">
-												<p class="skill-name">Lorem ipsum dolor</p>
-												<div class="sp cv5-sp">
-													<div class="sb cv5-sb w-80"></div>
-												</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="skills-info-li">
-												<p class="skill-name">Lorem ipsum dolor</p>
-												<div class="sp cv5-sp">
-													<div class="sb cv5-sb w-40"></div>
-												</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="skills-info-li">
-												<p class="skill-name">Lorem ipsum dolor</p>
-												<div class="sp cv5-sp">
-													<div class="sb cv5-sb w-40"></div>
-												</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="skills-info-li">
-												<p class="skill-name">Lorem ipsum dolor</p>
-												<div class="sp cv5-sp">
-													<div class="sb cv5-sb w-80"></div>
-												</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="skills-info-li">
-												<p class="skill-name">Lorem ipsum dolor</p>
-												<div class="sp cv5-sp">
-													<div class="sb cv5-sb w-40"></div>
-												</div>
-										</td>
-									</tr>
-								</table>
-							</div>
-						</div>
-					</div>
-					<!--Skills end-->
+<?php if(empty($pdetails['facebook']) && empty($pdetails['twitter']) && empty($pdetails['instagram']) && empty($pdetails['linkedin'])): else:?>
+<!--Social -->
+<div class="gi">
+<div class="info-header">
+<h2>Social</h2>
+<div class="header-line cv5-header-line"></div>
+</div>
+<div class="info-text cv5-info-text">
+<div class="cv5-social">
+<?php if(empty($pdetails['linkedin'])):else:?>
+<div class="opi-item cv5-s-opi-item">
+<p class="opi-item-title"><img src="./img/linkedin-b.png"></p>
+<p><?php echo $pdetails['linkedin'];?></p>
+</div>
+<?php endif;?>
 
-					<!--Social -->
-					<div class="gi">
-						<div class="info-header">
-							<h2>Social</h2>
-							<div class="header-line cv5-header-line"></div>
-						</div>
-						<div class="info-text cv5-info-text">
-							<div class="cv5-social">
-								<div class="opi-item cv5-s-opi-item">
-									<p class="opi-item-title"><img src="./img/linkedin-b.png"></p>
-									<p>Account link</p>
-								</div>
+<?php if(empty($pdetails['facebook'])):else:?>
+<div class="opi-item cv5-s-opi-item">
+<p class="opi-item-title"><img src="./img/facebook-b.png"></p>
+<p><?php echo $pdetails['facebook'];?></p>
+</div>
+<?php endif;?>
 
-								<div class="opi-item cv5-s-opi-item">
-									<p class="opi-item-title"><img src="./img/facebook-b.png"></p>
-									<p>Account link</p>
-								</div>
-								<div class="opi-item cv5-s-opi-item">
-									<p class="opi-item-title"><img src="./img/twitter-b.png"></p>
-									<p>Account link</p>
-								</div>
-								<div class="opi-item cv5-s-opi-item">
-									<p class="opi-item-title"><img src="./img/instagram-b.png"></p>
-									<p>Account link</p>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!--Social end-->
-				</div>
-				<!-- Bigger container at the right end -->
-			</div>
-		<!--Bigger container for Other details end-->
-		</div>
-	</div>
+<?php if(empty($pdetails['twitter'])):else:?>
+<div class="opi-item cv5-s-opi-item">
+<p class="opi-item-title"><img src="./img/twitter-b.png"></p>
+<p><?php echo $pdetails['twitter'];?></p>
+</div>
+<?php endif;?>
+
+<?php if(empty($pdetails['instagram'])):else:?>
+<div class="opi-item cv5-s-opi-item">
+<p class="opi-item-title"><img src="./img/instagram-b.png"></p>
+<p><?php echo $pdetails['instagram'];?></p>
+</div>
+<?php endif;?>
+</div>
+</div>
+</div>
+<?php endif;?>
+<!--Social end-->
+</div>
+<!-- Bigger container at the right end -->
+</div>
+<!--Bigger container for Other details end-->
+</div>
+</div>
 </body>
 </html>
+<?php else:endif;?>
