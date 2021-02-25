52efc0167c36269931dda4309a020e47bc246431
netsage <isaiahtokunbo11@gmail.com>
2021-02-25 10:59:09 +0100

    save

--- src/routes/api.php
+++ src/routes/api.php
@@ -53,12 +53,12 @@ endif;
 
 /*========================= Headings =========================*/
 $app->get('/email/test', function (Request $request, Response $response,array $args) {
-  $api = new Apiclass();    
+$api = new Apiclass();    
 
-  $SesClient = new SesClient([
-    'profile' => 'default',
-    'version' => '2010-12-01',
-    'region'  => 'us-east-2'
+$SesClient = new SesClient([
+'profile' => 'default',
+'version' => '2010-12-01',
+'region'  => 'us-east-2'
 ]);
 
 // Replace sender@example.com with your "From" address.
@@ -77,49 +77,49 @@ $recipient_emails = ['netsage23@gmail.com','info@speedcv.net'];
 $subject = 'Amazon SES test (AWS SDK for PHP)';
 $plaintext_body = 'This email was sent with Amazon SES using the AWS SDK for PHP.' ;
 $html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
-              '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
-              'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
-              'AWS SDK for PHP</a>.</p>';
+'<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+'AWS SDK for PHP</a>.</p>';
 $char_set = 'UTF-8';
 
 try {
-    $result = $SesClient->sendEmail([
-        'Destination' => [
-            'ToAddresses' => $recipient_emails,
-        ],
-        'ReplyToAddresses' => [$sender_email],
-        'Source' => $sender_email,
-        'Message' => [
-          'Body' => [
-              'Html' => [
-                  'Charset' => $char_set,
-                  'Data' => $html_body,
-              ],
-              'Text' => [
-                  'Charset' => $char_set,
-                  'Data' => $plaintext_body,
-              ],
-          ],
-          'Subject' => [
-              'Charset' => $char_set,
-              'Data' => $subject,
-          ],
-        ],
-        // If you aren't using a configuration set, comment or delete the
-        // following line
-        //'ConfigurationSetName' => $configuration_set,
-    ]);
-    $messageId = $result['MessageId'];
-    echo("Email sent! Message ID: $messageId"."\n");
+$result = $SesClient->sendEmail([
+'Destination' => [
+'ToAddresses' => $recipient_emails,
+],
+'ReplyToAddresses' => [$sender_email],
+'Source' => $sender_email,
+'Message' => [
+'Body' => [
+'Html' => [
+'Charset' => $char_set,
+'Data' => $html_body,
+],
+'Text' => [
+'Charset' => $char_set,
+'Data' => $plaintext_body,
+],
+],
+'Subject' => [
+'Charset' => $char_set,
+'Data' => $subject,
+],
+],
+// If you aren't using a configuration set, comment or delete the
+// following line
+//'ConfigurationSetName' => $configuration_set,
+]);
+$messageId = $result['MessageId'];
+echo("Email sent! Message ID: $messageId"."\n");
 } catch (AwsException $e) {
-    // output error message if fails
-    echo $e->getMessage();
-    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
-    echo "\n";
+// output error message if fails
+echo $e->getMessage();
+echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+echo "\n";
 }
-  
-  });
-  /*========================= Headings ENDS =========================*/
+
+});
+/*========================= Headings ENDS =========================*/
 
 
 
@@ -157,11 +157,11 @@ $subject = "Reset Password";
 $body = "<b style='text-align:center;color:#f37c20;'>Speed CV</b><br/><br/>Hi, we heard you lost Your Password, Here is Your Password Reset Code<br/> <b style='font-size:25px;'>$code</b>";
 $headers = array ('From' => $from, 'To' => $email ,'Subject' => $subject,"MIME-Version" => "1.0", "Content-type" => "text/html");
 $smtp = Mail::factory('smtp',
- array ('host' => $host,
-   'port' => $port,
-   'auth' => true,
-   'username' => $username,
-   'password' => $password));
+array ('host' => $host,
+'port' => $port,
+'auth' => true,
+'username' => $username,
+'password' => $password));
 $mail = $smtp->send($email, $headers, $body);
 
 if(PEAR::isError($mail)):
